4fbe1140bc1ede72445b1556cdaff1ac8dc4e760
lucian.li <731783128@qq.com>
2018-05-22 22:01:59 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,104 @@
+# name = input("What's your name:")
+# print("Hello"  , name)
+
+#username = input("Username:")
+#password = input("Password:")
+
+#输入账号密码
+
+
+# name = input("name:")
+# age = int(  input("age:")   )
+# job = input("job:")
+# hometown = input("hometown:")
+# print(type(name) , type(age))
+# info ="""
+# ------ info of %s------
+# Name:       %s
+# Age:        %d
+# Job:        %s
+# HomeTown;   %s
+# ------ End -------------
+# """%(name,name,age,job,hometown)
+#
+# print(info)
+
+# age = 50
+# realage = int( input ("Age:"))
+# if age > realage :
+#     print("Too Old")
+# else :
+#     print("======= Too young")
+#
+# print("_________________-----------_________")
+#
+# Username = input("Username:")
+# Password = input("Password:")
+#
+# _Username = "test"
+# _Password = "1234"
+#
+# if Username == _Username and Password == _Password :
+#     print("login successful")
+# else :
+#     print("Fail")
+
+# name = input( "Name:" )
+# sex = input("Sex:")
+# age = int( input("Age:") )
+#
+# if sex == "女" and age < 28:
+#     print("我喜欢女生")
+# else :
+#     if sex == "男" :
+#         print("一起来搞基")
+#     else :
+#         print("姐弟恋也很好哦")
+
+#
+# score = int( input ("Pls Scan Your Score:") )
+#
+# if score > 100 :
+#     print( "成绩最多到100." )
+# elif score >= 90 :
+#     print( "Level A" )
+# elif score >= 80 :
+#     print( "Level B" )
+# elif score >= 60 :
+#     print( "Level C" )
+# elif score >= 40 :
+#     print( "Level D" )
+# elif score >= 0 :
+#     print( "Level E" )
+
+# count = 0
+# while count <= 100 :
+#     if count >= 60 and count <= 80 :
+#         print( "loop", count**2)
+#     else:
+#         print("loop" , count)
+#     count += 1
+
+
+
+# count = 1
+# while count <= 3 :
+#
+#     age = int(input("Pls guess my name:"))
+#     if age == 21 :
+#         print("Yes")
+#         break
+#
+#
+#     if count == 3 :
+#         choice = input("Continue or not  Y or N")
+#
+#         if choice == 'Y' :
+#             print("You can guess again")
+#             count = 0
+#     print(count)
+#     count += 1
+# 
+
+import  pymongo
+print( 'welcom to')
